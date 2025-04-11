9925982f0902fb1615893f91e11a9190f6c119e8
Rakesh002 <rakesh@nerdplatoon.com>
2025-04-11 17:59:00 +0545

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,26 @@
-"use client"
 
 import { useContext, useEffect } from "react"
 
 import Slidder from "@/components/Slidder"
 import ProductList from "@/components/ProductList"
 import CategoryList from "@/components/CategoryList"
+import NewProductList from "@/components/NewProductList"
 import { useWixClient } from "@/hooks/useWixClient"
+import { Suspense } from "react"
 
 const HomePage = () => {
 
-  const wixClient = useWixClient();
+  // const wixClient = useWixClient();
 
-  const getProducts = async () => {
-    const res = await  wixClient.products.queryProducts().find();
-    console.log(res)
-  }
+  // const getProducts = async () => {
+  //   const res = await  wixClient.products.queryProducts().find();
+  //   console.log(res)
+  // }
 
-  useEffect(()=>{
-    getProducts();
+  // useEffect(()=>{
+  //   getProducts();
 
-  },[wixClient])
+  // },[wixClient])
 
   return (
     <div>
@@ -27,7 +28,12 @@ const HomePage = () => {
 
       <div className="mt-24 px-4 lg:px-8 xl:px-32 2xl:px-64">
         <h1 className="text-2xl">Featured Products</h1>
-        <ProductList />
+        <Suspense fallback={"Loading..."}>
+          <ProductList 
+            categoryId = {process.env.FEATURED_PRODUCT_CATEGORY_ID}
+            limit={4}
+          />
+        </Suspense>
       </div>
 
       <div className="mt-24">
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,124 +1,74 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
+import { wixClientServer } from '@/app/lib/wixClientServer'
+import { products } from '@wix/stores'
+import  DOMPurify  from 'isomorphic-dompurify'
 
-const ProductList = () => {
+const PRODUCT_PER_PAGE = 20
+
+
+const ProductList = async ({ categoryId,
+                            limit 
+        } : {  
+            categoryId : string, 
+            limit?: number 
+        }) => {
+            const wixClient = await wixClientServer();
+
+            const res = wixClient
+                .products
+                .queryProducts()
+                .eq("collectionIds", categoryId)
+                .limit(limit || PRODUCT_PER_PAGE)
+                .find()
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-                <div className="relative w-full h-80">
-                    <Image src="https://images.pexels.com/photos/31145707/pexels-photo-31145707/free-photo-of-black-and-white-blossoming-branches-in-spring.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                    />
-                    <Image src="https://images.pexels.com/photos/31508335/pexels-photo-31508335/free-photo-of-serene-waterfall-in-forest-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md"
-                    />
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">Price</span>
-                </div>
-                <div className="text-sm text-gray-500">My Description</div>
-                <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
-            </Link>
+            { (await res).items.map((product:products.Product)=>( 
+                <Link 
+                    href={"/"+product.slug} 
+                    className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
+                    key={product._id}
+                    >
+                    <div className="relative w-full h-80">
+                        <Image src={product.media?.mainMedia?.image?.url || "/product.png"} 
+                            alt="" 
+                            fill 
+                            sizes="25vw" 
+                            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                        />
 
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-                <div className="relative w-full h-80">
-                    <Image src="https://images.pexels.com/photos/31145707/pexels-photo-31145707/free-photo-of-black-and-white-blossoming-branches-in-spring.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
+                        { product.media?.items && (
+                            <Image src={product.media?.items[1]?.image?.url || "/product.png"}  
+                            alt="" 
+                            fill 
+                            sizes="25vw" 
+                            className="absolute object-cover rounded-md"
+                        /> 
+                        )}
+                        
+                    </div>
+                    <div className="flex justify-between ">
+                        <span className="font-medium">{product.name}</span>
+                        <span className="font-semibold">{product.priceData?.price}</span>
+                    </div>
+                    {product?.additionalInfoSections?.some(section => section.title === "shortDesc") && (
+                    <div
+                        className="text-sm text-gray-500"
+                        dangerouslySetInnerHTML={{
+                        __html: DOMPurify?.sanitize(
+                            product.additionalInfoSections.find(
+                            (section: any) => section.title === "shortDesc"
+                            )?.description || " "
+                        ),
+                        }}
                     />
-                    <Image src="https://images.pexels.com/photos/31508335/pexels-photo-31508335/free-photo-of-serene-waterfall-in-forest-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md"
-                    />
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">Price</span>
-                </div>
-                <div className="text-sm text-gray-500">My Description</div>
-                <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
-            </Link>
+                    )}
+                    <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
+                </Link>
+            ))}
 
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-                <div className="relative w-full h-80">
-                    <Image src="https://images.pexels.com/photos/31145707/pexels-photo-31145707/free-photo-of-black-and-white-blossoming-branches-in-spring.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                    />
-                    <Image src="https://images.pexels.com/photos/31508335/pexels-photo-31508335/free-photo-of-serene-waterfall-in-forest-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md"
-                    />
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">Price</span>
-                </div>
-                <div className="text-sm text-gray-500">My Description</div>
-                <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
-            </Link>
 
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-                <div className="relative w-full h-80">
-                    <Image src="https://images.pexels.com/photos/31145707/pexels-photo-31145707/free-photo-of-black-and-white-blossoming-branches-in-spring.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                    />
-                    <Image src="https://images.pexels.com/photos/31508335/pexels-photo-31508335/free-photo-of-serene-waterfall-in-forest-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md"
-                    />
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">Price</span>
-                </div>
-                <div className="text-sm text-gray-500">My Description</div>
-                <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
-            </Link>
-
-            <Link href="/test" className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]">
-                <div className="relative w-full h-80">
-                    <Image src="https://images.pexels.com/photos/31145707/pexels-photo-31145707/free-photo-of-black-and-white-blossoming-branches-in-spring.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-                    />
-                    <Image src="https://images.pexels.com/photos/31508335/pexels-photo-31508335/free-photo-of-serene-waterfall-in-forest-landscape.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" 
-                        alt="" 
-                        fill 
-                        sizes="25vw" 
-                        className="absolute object-cover rounded-md"
-                    />
-                </div>
-                <div className="flex justify-between ">
-                    <span className="font-medium">Product Name</span>
-                    <span className="font-semibold">Price</span>
-                </div>
-                <div className="text-sm text-gray-500">My Description</div>
-                <button className="rounded-2xl ring-1 ring-rakesh w-max py-2 px-4 text-xs hover:bg-rakesh hover:text-white">Add to Cart</button>
-            </Link>
         </div>
     )
 }
