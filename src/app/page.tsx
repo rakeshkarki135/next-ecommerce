f944e705ce5b068f366e1a83e8b5d372abdda717
office <rakesh@nerdplatoon.com>
2025-04-11 11:23:48 +0545

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+"use client"
+
+import { useContext, useEffect } from "react"
+
 import Slidder from "@/components/Slidder"
 import ProductList from "@/components/ProductList"
 import CategoryList from "@/components/CategoryList"
-import NewProductList from "@/components/NewProductList"
+import { useWixClient } from "@/hooks/useWixClient"
 
 const HomePage = () => {
+
+  const wixClient = useWixClient();
+
+  const getProducts = async () => {
+    const res = await  wixClient.products.queryProducts().find();
+    console.log(res)
+  }
+
+  useEffect(()=>{
+    getProducts();
+
+  },[wixClient])
+
   return (
     <div>
       <Slidder />
